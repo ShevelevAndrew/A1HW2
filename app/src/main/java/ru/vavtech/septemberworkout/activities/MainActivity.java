4298e31b2c4ff9fbcb1968921465c7c94222a1bc
Andrew <sit-surgut@mail.ru>
2018-10-31 23:25:17 +0500

    MainActivity

--- app/src/main/java/ru/vavtech/septemberworkout/activities/MainActivity.java
+++ app/src/main/java/ru/vavtech/septemberworkout/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.vavtech.septemberworkout.activities;
 
+import android.content.res.Configuration;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -12,26 +13,46 @@ import ru.vavtech.septemberworkout.interfaces.OnListItemClickListener;
 
 public class MainActivity extends AppCompatActivity implements OnListItemClickListener {
     FragmentManager fragmentManager;
+    WorkoutListFragment listFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WorkoutListFragment listFragment = new WorkoutListFragment();
+        listFragment = new WorkoutListFragment();
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.container, listFragment);
-        transaction.commit();
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            transaction.replace(R.id.container, listFragment);
+            transaction.commit();
+        }else {
+            WorkoutDetailFragment detailFragment = WorkoutDetailFragment.initFragment(0);
+            transaction.add(R.id.list_container, listFragment);
+            transaction.add(R.id.detail_container, detailFragment);
+            transaction.commit();
+        }
+
 
     }
 
     @Override
     public void onListItemClickListener(int index) {
         WorkoutDetailFragment detailFragment = WorkoutDetailFragment.initFragment(index);
-        fragmentManager.beginTransaction()
-                .add(R.id.container, detailFragment)
-                .addToBackStack("")
-                .commit();
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.container, detailFragment)
+                    .addToBackStack("")
+                    .commit();
+        }else {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.list_container, listFragment)
+                    .replace(R.id.detail_container, detailFragment)
+                    .commit();
+
+        }
     }
 }
